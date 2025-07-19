fe8b3a1c67a7d47977750122cead0e5cdc9beda5
AmirCHaal (WSL) <mohamedamir.chaal@gmail.com>
2025-07-19 10:40:02 +0200

    import avy model

--- src/components/AvyHead/AvyHead.tsx
+++ src/components/AvyHead/AvyHead.tsx
@@ -1,27 +1,32 @@
-import { Float } from "@react-three/drei"
+import { Float, useGLTF } from "@react-three/drei"
 import { useFrame } from "@react-three/fiber"
-import { useEffect, useRef } from "react"
+import { useRef, type RefObject } from "react"
 import { useGlobal } from "../../stores/global"
-import { div } from "three/tsl"
+import { Vector3, type Mesh } from "three"
 
 export default function AvyHead() {
-   const head = useRef(null)
-   const cursorFollower = useRef(null)
+   const robavyModel = useGLTF('robavy.glb')
+
+   const head = useRef({}) as RefObject<Mesh>
+   const cursorFollower = useRef({}) as RefObject<Mesh>
 
    const { cursorCoordinates, canvasSize } = useGlobal()
 
-   useFrame((state) => {
+   useFrame(() => {
       const followerX = cursorCoordinates.x / canvasSize.width
       const followerY = cursorCoordinates.y / canvasSize.height
-      const followerCoords = {
-         x: followerX,
-         y: followerY,
-         z: 3
-      }
+      const followerCoords = new Vector3(
+         followerX - 0.5,
+         -followerY + 0.5,
+         0.5 // edit this variable to ajust rotation intensity
+      )
 
-      cursorFollower.current.position.x = (followerCoords.x - 0.5) * 0.4
-      cursorFollower.current.position.y = (-followerCoords.y + 0.5) * 0.2
-      cursorFollower.current.position.z = followerCoords.z
+
+      console.log([...followerCoords])
+      cursorFollower.current.position.copy(followerCoords)
+      // cursorFollower.current.position.x = followerCoords.x
+      // cursorFollower.current.position.y = followerCoords.y
+      // cursorFollower.current.position.z = followerCoords.z
 
       head.current.lookAt(cursorFollower.current.position)
    })
@@ -30,16 +35,17 @@ export default function AvyHead() {
       <>
          {/* Cursor follower */}
          <mesh ref={cursorFollower} position={[0, 0, 4.8]}  >
-            <dodecahedronGeometry args={[0.1, 1]} />
-            <meshStandardMaterial color="blue" />
+            {/* <dodecahedronGeometry args={[0.1, 1]} />
+            <meshStandardMaterial color="blue" /> */}
          </mesh>
 
          {/* Avy head */}
-         <Float speed={2} rotationIntensity={0.5} floatIntensity={0.2} floatingRange={[-0.1, 0.1]} >
-            <mesh ref={head} position={[0, 0, 2]}>
+         <Float speed={2.5} rotationIntensity={0.5} floatIntensity={0.2} floatingRange={[-0.1, 0.1]} >
+            <primitive ref={head} object={robavyModel.scene} />;
+            {/* <mesh ref={head} position={[0, 0, 2]}>
                <boxGeometry />
                <meshStandardMaterial color="red" />
-            </mesh>
+            </mesh> */}
          </Float>
       </>
    )
