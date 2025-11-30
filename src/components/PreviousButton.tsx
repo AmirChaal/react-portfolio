dfeede566e5e98645c4b7e602610fbc31f9ad345
AmirCHaal (WSL) <mohamedamir.chaal@gmail.com>
2025-11-30 20:36:46 +0100

    fix incorrect typing of ComponentPropsWithoutRef in PreviousButton component

--- src/components/PreviousButton.tsx
+++ src/components/PreviousButton.tsx
@@ -2,7 +2,7 @@ import type { ComponentPropsWithoutRef } from "react";
 import PixelArrowIcon from "./icons/PixelArrowIcon";
 import StaticIconButton from "./StaticIconButton";
 
-export default function PreviousButton(wrapperProps: ComponentPropsWithoutRef<'div'>) {
+export default function PreviousButton(wrapperProps: ComponentPropsWithoutRef<'button'>) {
    return (
       <StaticIconButton {...wrapperProps} iconComponent={<PixelArrowIcon className="rotate-180" />} />
    )
